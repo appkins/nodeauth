87d1df34b51136c20062490c64cb3eadfa54a015
helldriver <nbatkins@gmail.com>
2017-03-19 05:23:02 -0500

    DB settings
    config change

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://<dbuser>:<dbpassword>@ds135680.mlab.com:35680/heroku_jrxw0s3v' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
